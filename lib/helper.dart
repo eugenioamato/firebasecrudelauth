cf2186bfe23a423dabb416947f621c26445641d7
Eugenio Amato <eugenioama@hotmail.com>
2021-02-07 16:50:57 +0100

    completed the helper class

--- lib/helper.dart
+++ lib/helper.dart
@@ -7,10 +7,10 @@ class Helper
   static bool _loading=false;
 
   // ignore: invalid_use_of_protected_member
-  static void startLoading(State<HomePage> s)=> s.setState((){_loading=true;});
+  static void startLoading(State s)=> s.setState((){_loading=true;});
 
   // ignore: invalid_use_of_protected_member
-  static void stopLoading(State<HomePage> s)=> s.setState((){_loading=false;});
+  static void stopLoading(State s)=> s.setState((){_loading=false;});
 
   static bool isLoading() => _loading;
 }
