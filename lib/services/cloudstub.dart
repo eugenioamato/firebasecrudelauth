4a3e6f9d7a03d9a4b47c0ca03457ad57c4156528
Eugenio Amato <eugenioama@hotmail.com>
2021-02-10 22:37:23 +0100

    put a callback in the helper method

--- lib/services/cloudstub.dart
+++ lib/services/cloudstub.dart
@@ -1,13 +1,10 @@
-
 class FirebaseFirestore {
   static FirebaseFirestore instance;
   collection(String s) {}
 }
 class QuerySnapshot {
   var docs;
-
 }
-
 class QueryDocumentSnapshot {
 var data;
 var id;
